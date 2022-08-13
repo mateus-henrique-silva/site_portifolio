ad8d35a4d5debbc36f0034e130f21fe22c9fc30a
“mateus-henrique-silva” <mhtecdev@gmail.com>
2022-08-13 16:40:20 -0300

    Upando site portifolio em construção

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Projects from "./Pages/Projects/Projects";
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <nav>
+          <ul>
+            <li>GitHub</li>
+            <li>Linkedin</li>
+            <li>Instagram</li>
+          </ul>
+        </nav>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
